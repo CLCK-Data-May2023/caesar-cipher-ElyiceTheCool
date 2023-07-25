f8cbfd0b91deb32a0e0ccb7d1296c5a1ba914f31
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-25 19:44:09 -0400

    segmented problem

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+# Get user sentence as input
+user_sentence = input("Please enter a sentence: ")
+user_char = list(user_sentence)
+
+# Get character index of each letter in sentence
+abc = "abcdefghijklmnopqrstuvwxyz"
+
+def get_char_index():
+    char_index = abc.find(user_char)
+    
+
+# Encrypt by moving index to the right 5 spaces
+
+encrypted_sentence = []
+
+# Print full encrypted sentence
+
